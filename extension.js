027526d714ca4358247ed6adb30bcfd757a07a73
ctf0 <muah003@gmail.com>
2019-12-29 18:43:20 +0200

    wait for it

--- extension.js
+++ extension.js
@@ -4,7 +4,7 @@ const vscode = require('vscode')
  * @param {vscode.ExtensionContext} context
  */
 function activate(context) {
-    let disposable = vscode.commands.registerCommand('smart.unindent', () => {
+    let disposable = vscode.commands.registerCommand('smart.unindent', async () => {
         let editor = vscode.window.activeTextEditor
         const { document, selections } = editor
 
@@ -20,12 +20,12 @@ function activate(context) {
             let txt = document.getText(range)
 
             if (/\S\s{2,}/.test(txt)) {
-                editor.edit(
+                await editor.edit(
                     (edit) => edit.replace(range, txt.replace(/\S\s{2,}/, (match) => match.trim())),
                     { undoStopBefore: false, undoStopAfter: false }
                 )
             } else {
-                editor.edit(
+                await editor.edit(
                     (edit) => edit.replace(range, txt.replace(new RegExp(`^\\s{${tab_size}}`), '')),
                     { undoStopBefore: false, undoStopAfter: false }
                 )
