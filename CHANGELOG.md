87fe8640c4efa31ef5c119b5a63522da0a033636
ctf0 <muah003@gmail.com>
2020-01-19 10:55:16 +0200

    004

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -7,3 +7,8 @@
 ## 0.0.2
 
 - fix cant unindent when indentation first level is 0
+
+## 0.0.4
+
+- new config `removeExtraSpace`
+- fix behavior with multiple white spaces positions on the same line
--- extension.js
+++ extension.js
@@ -1,43 +1,79 @@
 const vscode = require('vscode')
+const PACKAGE_NAME = 'smart-unindent'
+
+let config = {}
 
 /**
  * @param {vscode.ExtensionContext} context
  */
-function activate(context) {
-    let disposable = vscode.commands.registerCommand('smart.unindent', async () => {
-        let editor = vscode.window.activeTextEditor
-        const { document, selections } = editor
-        let done = 0
+async function activate(context) {
+    await readConfig()
 
-        for (const selection of selections) {
-            let tab_size = editor.options.tabSize
-            const { start, end } = selection
-
-            let range = new vscode.Range(
-                start.line,
-                0,
-                end.line,
-                end.character
-            )
-            let txt = document.getText(range)
-            let regex = /\S\s{2,}/
-
-            if (range.isSingleLine && regex.test(txt)) {
-                done++
-                await editor.edit(
-                    (edit) => edit.replace(range, txt.replace(regex, (match) => match.trim())),
-                    { undoStopBefore: false, undoStopAfter: false }
-                )
-            }
-        }
-
-        if (selections.length > done) {
-            await vscode.commands.executeCommand('editor.action.outdentLines')
+    vscode.workspace.onDidChangeConfiguration(async (e) => {
+        if (e.affectsConfiguration(PACKAGE_NAME)) {
+            await readConfig()
         }
     })
 
-    context.subscriptions.push(disposable)
+    context.subscriptions.push(
+        vscode.commands.registerCommand('smart.unindent', async () => {
+            let editor = vscode.window.activeTextEditor
+            let { document, selections } = editor
+            let done = 0
+
+            for (const selection of selections) {
+                if (selection.isSingleLine) {
+                    let txt
+                    let range
+                    let regex
+                    const { start, end } = selection
+
+                    if (atStartOfLine(selection) && config.removeExtraSpace) {
+                        regex = new RegExp(/\S\s{2,}/)
+                        txt = document.lineAt(start.line).text
+                        range = getRange(selection, txt.length)
+                    } else {
+                        regex = new RegExp(/(\S\s{2,})$/, 'g')
+                        range = getRange(selection, end.character)
+                        txt = document.getText(range)
+                    }
+
+                    if (regex.test(txt)) {
+                        done++
+                        await editor.edit(
+                            (edit) => edit.replace(range, txt.replace(regex, (match) => match.trim())),
+                            { undoStopBefore: false, undoStopAfter: false }
+                        )
+                    }
+                }
+            }
+
+            if (selections.length > done) {
+                await vscode.commands.executeCommand('editor.action.outdentLines')
+            }
+        })
+    )
 }
+
+function getRange(selection, endLength) {
+    const { start, end } = selection
+
+    return new vscode.Range(
+        start.line,
+        0,
+        end.line,
+        endLength
+    )
+}
+
+function atStartOfLine(selection) {
+    return selection.start.character == 0 && selection.end.character == 0
+}
+
+async function readConfig() {
+    return config = await vscode.workspace.getConfiguration(PACKAGE_NAME)
+}
+
 exports.activate = activate
 
 function deactivate() { }
